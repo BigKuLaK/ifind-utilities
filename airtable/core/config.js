3d62ada34922176c36790955c549ba9c8c544e61
Jayson Buquia <jayson.buquia@wearekettle.com>
2023-02-01 18:58:18 +0800

    Update config to use env

--- airtable/core/config.js
+++ airtable/core/config.js
@@ -1,8 +1,4 @@
-// TODO: Move these to /.env
-const AIRTABLE_API_KEY = "keyEA4usdufwK4na2"; // Jayson's API_KEY
-const AIRTABLE_BASE_SCHEDULED_TASKS = "appNWlIhbI2h4pBGy"; // scheduled-tasks-dev base
-
 module.exports = {
-  AIRTABLE_API_KEY,
-  AIRTABLE_BASE_SCHEDULED_TASKS,
+  AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+  AIRTABLE_BASE_SCHEDULED_TASKS: process.env.AIRTABLE_BASE_SCHEDULED_TASKS,
 };
