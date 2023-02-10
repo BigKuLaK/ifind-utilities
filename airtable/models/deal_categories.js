3136ffd45cd1a360bd1f69ae13a509b1860f8ae4
Jayson Buquia <jayson.buquia@wearekettle.com>
2023-02-10 17:08:37 +0800

    Update deal_type and deal_categories

--- airtable/models/deal_categories.js
+++ airtable/models/deal_categories.js
@@ -11,6 +11,31 @@ class DealTypes extends Model {
   static base = "scheduledTasks";
 
   static table = "deal_categories";
+
+  static async allData() {
+    const dealTypes = await this.all().then((records) =>
+      records.map(({ fields }) => ({
+        ...fields,
+        label: this.constructLabels(fields),
+      }))
+    );
+
+    return dealTypes;
+  }
+
+  static constructLabels(dealCategoryFields) {
+    const labels = [];
+
+    Object.keys(dealCategoryFields).forEach((field) => {
+      if (/label_/.test(field)) {
+        const language = field.split("_").pop();
+        const label = dealCategoryFields[field];
+        labels.push({ language, label });
+      }
+    });
+
+    return labels;
+  }
 }
 
 module.exports = DealTypes;
--- airtable/models/deal_types.js
+++ airtable/models/deal_types.js
@@ -11,6 +11,45 @@ class DealTypes extends Model {
   static base = "scheduledTasks";
 
   static table = "deal_types";
+
+  static async allData() {
+    const dealTypes = await this.all().then((records) =>
+      records.map(({ fields }) => ({
+        ...fields,
+        ...this.constructLabels(fields),
+      }))
+    );
+    return dealTypes;
+  }
+
+  static constructLabels(dealTypeFields) {
+    const labels = {
+      /**@type {Record<string, string>[]} */
+      nav_label: [],
+      /**@type {Record<string, string>[]} */
+      label: [],
+    };
+
+    Object.keys(dealTypeFields).forEach((field) => {
+      switch (true) {
+        case /^nav_label_/.test(field):
+          labels.nav_label.push({
+            language: /**@type {string} */ (field.split("_").pop()),
+            label: dealTypeFields[field],
+          });
+          break;
+        case /^section_label_/.test(field):
+          labels.label.push({
+            language: /**@type {string} */ (field.split("_").pop()),
+            label: dealTypeFields[field],
+          });
+          break;
+        default:
+      }
+    });
+
+    return labels;
+  }
 }
 
 module.exports = DealTypes;
